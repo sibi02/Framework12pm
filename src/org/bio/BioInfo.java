ea781562d711de42874ebfdaaf5c6230d09bc270
sibi02 <sibiraj5000@gmail.com>
2021-10-26 12:56:56 +0530

    I push the code

---
+++ src/org/bio/BioInfo.java
@@ -0,0 +1,43 @@
+package org.bio;
+
+public class BioInfo {
+	public static void main(String[] args) {
+		String myName = "S.Sibi";
+		System.out.println("Name : " + myName);
+		String dob = "30/11/1998";
+		System.out.println("D.O.B : " + dob);
+		byte age = 22;
+		System.out.println("Age : " + age);
+		char sex = 'M';
+		System.out.println("Sex : " + sex);
+		String email = "sibiraj5000@gmail.com";
+		System.out.println("Email.Id : " + email);
+		short code = 9204;
+		System.out.println("College.Code: " + code);
+		int reg = 167489025;
+		System.out.println("Univ.RegNo : " + reg);
+		String fName = "C.R.Suresh";
+		System.out.println("Father's Name : " + fName);
+		String mName = "S.Kousalya";
+		System.out.println("Mother's Nmae : " + mName);
+		String bName = "S.Saai";
+		System.out.println("Brother Name: " + bName);
+		String martial = "Single";
+		System.out.println("MartialStatus : " + martial);
+		String nation = "India";
+		System.out.println("Nationality : " + nation);
+		long phNo = 465321789753l;
+		System.out.println("PhoneNo : " + phNo);
+		double sslc = 89.54658;
+		System.out.println("SSLC_AVG : " + sslc);	
+		double hsc = 71.45675;
+		System.out.println("HSC_AVG : " + hsc);
+		double ug = 66.4875;
+		System.out.println("ENGG_AVG : " + ug);
+		float sal = 2200.5554454873f;
+		System.out.println("Expecting Salary/Month : Rs." + sal);
+		boolean captcha = true;
+		System.out.println("TCS is a MultiNational Company : " + captcha);
+	}
+
+}
